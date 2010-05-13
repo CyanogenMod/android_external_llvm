1feebb5df60f176a6338d45bef7cdf8b55aa2298
Shih-wei Liao <sliao@google.com>
2010-05-13 15:19:51 -0700

    Add "*2" to vstmia
    Change-Id: I95e6eda70555757ca9378d508d0470b5ef0f64b2

--- lib/Target/ARM/ARMCodeEmitter.cpp
+++ lib/Target/ARM/ARMCodeEmitter.cpp
@@ -1533,7 +1533,7 @@ ARMCodeEmitter::emitVFPLoadStoreMultipleInstruction(const MachineInstr &MI) {
       break;
     ++NumRegs;
   }
-  Binary |= NumRegs;
+  Binary |= NumRegs * 2;
 
   emitWordLE(Binary);
 }
