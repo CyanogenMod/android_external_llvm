d01bbbe41e176ebec2489463b19bcb7c41a43b2c
Ying Wang <wangying@google.com>
2010-09-08 09:54:08 -0700

    Remove llvm dependency on clang
    Change-Id: I1b50299710cbb726eb5ff3f227498bcac65c4f79

--- llvm.mk
+++ llvm.mk
@@ -9,7 +9,3 @@ LLVM_HOST_BUILD_MK := $(LLVM_ROOT_PATH)/llvm-host-build.mk
 LLVM_DEVICE_BUILD_MK := $(LLVM_ROOT_PATH)/llvm-device-build.mk
 LLVM_GEN_INTRINSICS_MK := $(LLVM_ROOT_PATH)/llvm-gen-intrinsics.mk
 LLVM_TBLGEN_RULES_MK := $(LLVM_ROOT_PATH)/llvm-tblgen-rules.mk
-
-CLANG_ROOT_PATH := external/clang
-
-include $(CLANG_ROOT_PATH)/clang.mk
