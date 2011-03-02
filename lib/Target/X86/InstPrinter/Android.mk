8519f22d51998efaed29fcb5ad647fa0b34faac9
Shih-wei Liao <sliao@google.com>
2011-03-02 06:44:55 -0800

    Add build rules for LLVMX86InstPrinter when TARGET_ARCH is x86.
    Change-Id: Ic71294eda24d4eb7b04fa4570bb0110a3d5d3dd4

--- lib/Target/X86/InstPrinter/Android.mk
+++ lib/Target/X86/InstPrinter/Android.mk
@@ -1,27 +1,54 @@
 LOCAL_PATH := $(call my-dir)
 
-# For the host only
-# =====================================================
-include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS)
-
-TBLGEN_TABLES := \
+x86_instprinter_TBLGEN_TABLES := \
   X86GenAsmWriter.inc \
   X86GenAsmWriter1.inc \
   X86GenInstrNames.inc \
   X86GenRegisterNames.inc \
   X86GenRegisterInfo.h.inc
 
-TBLGEN_TD_DIR := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := \
+x86_instprinter_SRC_FILES := \
   X86ATTInstPrinter.cpp \
   X86IntelInstPrinter.cpp \
   X86InstComments.cpp
 
+# For the device
+# =====================================================
+ifeq ($(TARGET_ARCH),x86)
+include $(CLEAR_VARS)
+include $(CLEAR_TBLGEN_VARS)
+
+TBLGEN_TABLES := $(x86_instprinter_TBLGEN_TABLES)
+
+TBLGEN_TD_DIR := $(LOCAL_PATH)/..
+
+LOCAL_SRC_FILES := $(x86_instprinter_SRC_FILES)
+
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
 LOCAL_MODULE:= libLLVMX86InstPrinter
+
+LOCAL_MODULE_TAGS := optional
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_TBLGEN_RULES_MK)
+include $(BUILD_STATIC_LIBRARY)
+endif
+
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
+include $(CLEAR_TBLGEN_VARS)
+
+TBLGEN_TABLES := $(x86_instprinter_TBLGEN_TABLES)
+
+TBLGEN_TD_DIR := $(LOCAL_PATH)/..
+
+LOCAL_SRC_FILES := $(x86_instprinter_SRC_FILES)
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
+
+LOCAL_MODULE := libLLVMX86InstPrinter
 
 LOCAL_MODULE_TAGS := optional
 
