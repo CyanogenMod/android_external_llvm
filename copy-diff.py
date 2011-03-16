2010bbd9e389c9745a03a21d1cfa87acacda6abb
Shih-wei Liao <sliao@google.com>
2011-03-15 19:11:51 -0700

    Fix the MAC build.
    Stop using Python 2.6 features.
    Don't use printf function or with statement.
    I think the code is uglier now.
    If MAC build machine problem can be fixed instead, the code can be more elegant.
    
    Change-Id: I434365ea8e1ca78ed83bfc31f8db5304b802392d

--- copy-diff.py
+++ copy-diff.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from __future__ import with_statement
-
 import os
 import shutil
 import sys
@@ -15,22 +12,29 @@ def is_file_different(a, b):
         return True
 
     # Read the content of the files, and compare them.
-    with open(a, 'rb') as fa:
-        with open(b, 'rb') as fb:
-            while True:
-                buff_a = fa.read(BUFFER_SIZE)
-                buff_b = fb.read(BUFFER_SIZE)
+    result = False
 
-                if buff_a != buff_b:
-                    # File is different in this block.
-                    return True
+    fa = open(a, 'rb')
+    fb = open(b, 'rb')
 
-                if len(buff_a) < BUFFER_SIZE:
-                    # Finished
-                    break
+    while True:
+        buff_a = fa.read(BUFFER_SIZE)
+        buff_b = fb.read(BUFFER_SIZE)
+
+        if buff_a != buff_b:
+            # File is different in this block.
+            result = True
+            break
+
+        if len(buff_a) < BUFFER_SIZE:
+            # Finished
+            break
+
+    fa.close()
+    fb.close()
 
     # File is the same.
-    return False
+    return result
 
 def copyfile(src, dest):
     if not os.path.exists(src):
@@ -53,19 +57,19 @@ def copyfile(src, dest):
 
 def main():
     if len(sys.argv) < 3:
-        print('USAGE:', sys.argv[0], '<srcfile> <destfile>', file=sys.stderr)
+        print >> sys.stderr, 'USAGE:', sys.argv[0], '<srcfile> <destfile>'
         sys.exit(1)
 
     srcfile = os.path.abspath(sys.argv[1])
     destfile = os.path.abspath(sys.argv[2])
 
     if srcfile == destfile:
-        print('WARNING: <srcfile> is equal to <destfile>', file=sys.stderr)
+        print >> sys.stderr, 'WARNING: <srcfile> is equal to <destfile>'
     else:
         try:
             copyfile(srcfile, destfile)
         except ValueError as e:
-            print('ERROR:', e, file=sys.stderr)
+            print >> sys.stderr, 'ERROR: ', e
             sys.exit(1)
 
 if __name__ == '__main__':
