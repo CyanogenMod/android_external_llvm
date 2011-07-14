7445072fa666b054015b797743e08c02557068e9
Stephen Hines <srhines@google.com>
2011-07-13 17:26:13 -0700

    Build BitcodeWriter for device as well.
    BUG=4278636
    
    Change-Id: Id16d7a74c19e8575af4391997bd226b26498053e

--- lib/Bitcode/Writer/Android.mk
+++ lib/Bitcode/Writer/Android.mk
@@ -1,14 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
 
-# For the host only
-# =====================================================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES :=	\
+bitcode_writer_SRC_FILES :=	\
 	BitWriter.cpp	\
 	BitcodeWriter.cpp	\
 	BitcodeWriterPass.cpp	\
 	ValueEnumerator.cpp
+
+# For the host
+# =====================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(bitcode_writer_SRC_FILES)
 
 LOCAL_MODULE:= libLLVMBitWriter
 
@@ -17,3 +19,17 @@ LOCAL_MODULE_TAGS := optional
 include $(LLVM_HOST_BUILD_MK)
 include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
+
+# For the device
+# =====================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(bitcode_writer_SRC_FILES)
+
+LOCAL_MODULE:= libLLVMBitWriter
+
+LOCAL_MODULE_TAGS := optional
+
+include $(LLVM_DEVICE_BUILD_MK)
+include $(LLVM_GEN_INTRINSICS_MK)
+include $(BUILD_STATIC_LIBRARY)
