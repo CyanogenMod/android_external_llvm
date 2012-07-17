4a05ed708aed4c7a099d924ed3feb604d3e44074
Shih-wei Liao <sliao@google.com>
2012-07-17 15:46:02 -0700

    Fix Mac build breakage: Restore the host build mk file here.
    Then, fix Mac build breakage on the libbcc side.
    
    Change-Id: I098c8524944e6d6feda1ebe80d926aebc6ba0c00

--- llvm-host-build.mk
+++ llvm-host-build.mk
@@ -1,5 +1,8 @@
 LOCAL_CFLAGS :=	\
 	-D_GNU_SOURCE	\
+	-D__STDC_LIMIT_MACROS	\
+	-D__STDC_CONSTANT_MACROS	\
+	-D__STDC_FORMAT_MACROS	\
 	-O2	\
 	-fomit-frame-pointer	\
 	-Wall	\
@@ -7,13 +10,6 @@ LOCAL_CFLAGS :=	\
 	-Wno-unused-parameter	\
 	-Wwrite-strings	\
 	$(LOCAL_CFLAGS)
-
-ifneq ($(HOST_OS),darwin)
-LOCAL_CFLAGS +=	\
-	-D__STDC_LIMIT_MACROS	\
-	-D__STDC_CONSTANT_MACROS	\
-	-D__STDC_FORMAT_MACROS
-endif
 
 ifeq ($(LLVM_ENABLE_ASSERTION),true)
 LOCAL_CFLAGS :=	\
