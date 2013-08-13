e4799f60dc79d16ee270b38e9fab805178a572aa
Stephen Hines <srhines@google.com>
2013-08-13 01:28:16 -0700

    Fix build break due to conflicting #define values.
    Change-Id: I502c13aba9eb720a9591af998cb339836c9d8620

--- device/include/llvm/Config/config.h
+++ device/include/llvm/Config/config.h
@@ -613,7 +613,7 @@
 #define LLVM_VERSION_MAJOR 3
 
 /* Minor version of the LLVM API */
-#define LLVM_VERSION_MINOR 1
+#define LLVM_VERSION_MINOR 3
 
 /* Define if the OS needs help to load dependent libraries for dlopen(). */
 /* #undef LTDL_DLOPEN_DEPLIBS */
